19efb64562ab3636ca31540dfdb325fe8f2e7352
gsFALCON <gsfalcon@proton.me>
2022-12-13 03:37:53 -0300

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,11 +1,11 @@
+# https://github.com/rany2/edge-tts
 
 import os
 import random
 
-texto = input("Por favor, digite o texto a ser convertido em áudio: ")
-
+texto = input("Digite o texto a ser convertido em .mp3: ")
 n_aleat = random.randint(1111, 9999)
 
+os.system('edge-tts --voice pt-BR-AntonioNeural --text "{}" --write-media audio_{}.mp3'.format(texto, n_aleat))
 
-
-os.system('edge-tts --voice pt-BR-AntonioNeural --text "{}" --write-media audio{}.mp3'.format(texto, n_aleat))
+print("\nArquivo de áudio salvo em: 'tts_python/audio_{}.mp3'\n".format(n_aleat))
