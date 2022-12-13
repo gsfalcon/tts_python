0347f894145612f3fb0345f2a910ddff01b36abf
gsFALCON <gsfalcon@proton.me>
2022-12-13 02:55:45 -0300

    update

--- tts.py
+++ tts.py
@@ -1,1 +1,11 @@
-edge-tts --voice pt-BR-AntonioNeural --text "$text" --write-media teste1.mp3
+
+import os
+import random
+
+texto = input("Por favor, digite o texto a ser convertido em áudio: ")
+
+n_aleat = random.randint(1111, 9999)
+
+
+
+os.system('edge-tts --voice pt-BR-AntonioNeural --text "{}" --write-media audio{}.mp3'.format(texto, n_aleat))
