411a8cf6bb5fd776fa06e2a2bc031bc69ec7966c
gsFALCON <gsfalcon@proton.me>
2022-12-13 06:28:22 -0300

    Update tts.py

--- tts.py
+++ tts.py
@@ -2,8 +2,11 @@ import os
 import random
 
 userText = input("Digite o texto a ser convertido em .mp3: ")
-idGen = random.randint(1111, 9999)
+idGen = random.randint(100, 999)
 
-os.system('edge-tts --voice pt-BR-AntonioNeural --text "{}" --write-media audio_{}.mp3'.format(userText, idGen))
+os.system('edge-tts --voice pt-BR-AntonioNeural --pitch=-5Hz --rate=0.97 --text "{}" --write-media audio_{}.mp3'.format(userText, idGen))
 
 print("\nArquivo de áudio salvo em: 'tts_python/audio_{}.mp3'\n".format(idGen))
+
+
+#remover line breaks and paragraph breaks no input text
