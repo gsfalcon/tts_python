5775e144087288544c96c7a4e6f966f733a6c4ac
Guilherme Souza <gsfalcon@proton.me>
2023-11-24 15:59:26 -0300

    idk

--- tts.py
+++ tts.py
@@ -4,9 +4,16 @@ import random
 userText = input("Digite o texto a ser convertido em .mp3: ")
 idGen = random.randint(100, 999)
 
-os.system('edge-tts --voice pt-BR-AntonioNeural --pitch=-5Hz --rate=0.97 --text "{}" --write-media audio_{}.mp3'.format(userText, idGen))
+cleanedText = userText.replace("\n", " ").replace("\r", " ")
 
-print("\nArquivo de áudio salvo em: '../tts_python/audio_{}.mp3'\n".format(idGen))
+# Utilize o caminho completo para evitar problemas de resolução de diretório
+output_path = os.path.abspath(os.path.join(os.path.dirname(__file__), f'../tts_python/audio_{idGen}.mp3'))
 
+try:
+    os.system(f'edge-tts --voice pt-BR-AntonioNeural --pitch=-5Hz --rate=0.97 --text "{cleanedText}" --write-media "{output_path}"')
+    print(f"\nArquivo de áudio salvo em: '{output_path}'\n")
+except Exception as e:
+    print("Erro ao salvar o arquivo de áudio:", e)
 
-#remover line breaks and paragraph breaks no input text
+# pip install pipx
+# pipx install edge-tts
